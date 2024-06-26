f82d3550f908c0e29100215cf5c1527777c03081
Bakkeby <bakkeby@gmail.com>
2024-06-26 09:41:35 +0200

    dragcfact patch
    The dragcfact patch allow you resize clients' size (i.e. modify cfact) by holding modkey + shift + right-click and dragging the mouse.

--- config.def.h
+++ config.def.h
@@ -119,6 +119,7 @@ static const Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask, Button3,      dragcfact,      {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
