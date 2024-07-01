b426d7c20e8a58a07396e4aa62ac13bfa7ac55b5
Bakkeby <bakkeby@gmail.com>
2024-07-01 23:15:17 +0200

    monitorrules patch
    Monitor rules patch on top of pertag
    
    This patch allows the user to define layout, mfact, nmaster, showbar,
    and topbar settings on a per monitor basis. An example use case could
    be to have a layout with a horizontal split for a secondary vertical
    monitor.
    
    https://github.com/bakkeby/patches/wiki/monitorrules

--- config.def.h
+++ config.def.h
@@ -45,6 +45,12 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+static const MonitorRule monrules[] = {
+	/* monitor  tag  layout  mfact  nmaster  showbar  topbar */
+	{  1,       -1,  2,      -1,    -1,      -1,      -1     }, // use a different layout for the second monitor
+	{  -1,      -1,  0,      -1,    -1,      -1,      -1     }, // default
+};
+
 /* key definitions */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
