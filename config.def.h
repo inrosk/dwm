8206c4db34e6a4f015b9d1b26448f15711826cb5
karol <kr@inrosk.com>
2024-09-25 17:55:42 +0200

    Run virt manager on tag 8

--- config.def.h
+++ config.def.h
@@ -56,7 +56,7 @@ static Sp scratchpads[] = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *defaulttagapps[] = { "firefox", "brave", "alacritty", "emacs", "figma-linux", "steam", "dmenu_run", "dmenu_run", "dmenu_run" };
+static const char *defaulttagapps[] = { "firefox", "brave", "alacritty", "emacs", "figma-linux", "steam", "dmenu_run", "virt-manager", "dmenu_run" };
 
 static const Rule rules[] = {
 	/* xprop(1):
